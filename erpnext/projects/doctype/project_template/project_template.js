1033b6679ae80f5ca53960d1167b5750073454db
Teqsys-erp <erp.support@teqsys.in>
2025-01-19 10:52:51 +0530

    Update project_template.js added custom field inside the js file also

--- erpnext/projects/doctype/project_template/project_template.js
+++ erpnext/projects/doctype/project_template/project_template.js
@@ -17,11 +17,14 @@ frappe.ui.form.on("Project Template", {
 });
 
 frappe.ui.form.on("Project Template Task", {
-	task: function (frm, cdt, cdn) {
-		var row = locals[cdt][cdn];
-		frappe.db.get_value("Task", row.task, "subject", (value) => {
-			row.subject = value.subject;
-			refresh_field("tasks");
-		});
-	},
+        task: function (frm, cdt, cdn) {
+                var row = locals[cdt][cdn];
+                frappe.db.get_value("Task", row.task, ["subject", "custom_activity_for", "custom_category"], (value) => {
+                        row.subject = value.subject;
+                        row.custom_activity_for = value.custom_activity_for;
+                        row.custom_category = value.custom_category;
+                        refresh_field("tasks");
+                });
+        },
 });
+
